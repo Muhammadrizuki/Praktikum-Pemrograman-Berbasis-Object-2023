8b0c08f5ea11fae0f3ca77ef9f2326f1bdecb796
Mahendra Kirana M.B <113653966+Mahen218@users.noreply.github.com>
2023-03-22 20:00:11 +0800

    Delete CuboidMain.java

--- Praktikum 1/H071221058/CuboidMain.java
+++
@@ -1,9 +0,0 @@
-public class CuboidMain {
-    public static void main(String[] args) {
-        Cuboid cuboid = new Cuboid();
-        cuboid.width(15.0);
-        cuboid.height(20.0);
-        cuboid.length(15.0);
-        System.out.printf("Volume = %.2f", cuboid.getVolume());
-    }
-}
