da45a79834e83c07582b6fb0595ea58fb764a8c9
Mahendra Kirana M.B <113653966+Mahen218@users.noreply.github.com>
2023-04-14 12:03:08 +0800

    Add files via upload

---
+++ Praktikum 4/H071221058/StringUtils.java
@@ -0,0 +1,12 @@
+package TUGAS.Pertemuan4;
+
+public class StringUtils {
+    public static String getNickname(String fullName) {
+        String[] names = fullName.split(" "); // digunakan untuk memecah sebuah string menjadi beberapa bagian dengan menggunakan karakter spasi 
+        if (names.length == 1) {   //method akan mengembalikan nilai parameter fullName itu sendiri sebagai nickname.
+            return fullName;
+        } else {
+            return names[1]; //method akan mengembalikan nilai elemen ke-2 (indeks ke-1) sebagai nickname.
+        }
+    }
+}
